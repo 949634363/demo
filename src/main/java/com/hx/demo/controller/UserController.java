5f9a7ae4dbec5df618f3e13686a94dbbc4ff24e6
lcy <lcy949634363@gmail.com>
2020-10-16 16:12:07 +0800

    登录验证

--- src/main/java/com/hx/demo/controller/UserController.java
+++ src/main/java/com/hx/demo/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.hx.demo.controller;
 
-import com.hx.demo.annotation.CheckLogin;
 import com.hx.demo.bean.model.AppUserMobile;
-import com.hx.demo.bean.vo.UserVo;
 import com.hx.demo.service.AppUserMobileService;
-import com.tencentcloudapi.iot.v20180123.models.AppUser;
+import com.hxcore.annotation.CheckLogin;
+import com.hxcore.base.BaseOperate;
+import com.hxcore.cache.CacheNameSpace;
+import com.hxcore.cache.CacheUtil;
+import com.hxcore.jwt.JWTToken;
+import com.hxcore.result.ResultDataType;
+import com.hxcore.result.ResultHx;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +28,18 @@ public class UserController {
     @Autowired
     private AppUserMobileService appUserMobileService;
 
-    @CheckLogin
     @RequestMapping("/login")
-    public String login(UserVo userVo) {
-        return "测试";
+    public ResultHx login(String mobile) {
+        String token = JWTToken.generateToken(mobile);
+        CacheUtil.put(CacheNameSpace.UserActivity, token, 1);
+        return ResultHx.getInstance().buildSuccess(BaseOperate.LOGIN).buildData(ResultDataType.RECORD, token);
+    }
+
+    @CheckLogin
+    @RequestMapping("/test")
+    public ResultHx test(@RequestHeader String token) {
+        String claims = JWTToken.getClaims(token);
+        return ResultHx.getInstance().buildSuccess("测试").buildData(ResultDataType.RECORD, claims);
     }
 
     @RequestMapping("/list")
--- src/main/java/com/hx/demo/util/Test.java
+++ src/main/java/com/hx/demo/util/Test.java
@@ -1,11 +1,16 @@
 package com.hx.demo.util;
 
 import com.alibaba.dubbo.rpc.RpcException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.time.DateUtils;
 
-import java.text.SimpleDateFormat;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Test
@@ -15,13 +20,42 @@ import java.util.Date;
  */
 @Slf4j
 public class Test {
-
+    private static final String SECRET = "qw@e#w$q%s^f^da!@#$%^&*()_+asdfghjkxcvbefidsklxncvisjdfsdf";
     static int i = 0;
 
     public Test() {
         log.error("TestError" + i++);
         log.info("TestInfo" + i++);
         throw new RpcException();
+    }
+
+    public static String generateToken(Map<String, Object> claims, String subject) {
+        SecretKeySpec key = new SecretKeySpec(SECRET.getBytes(), SignatureAlgorithm.HS512.getJcaName());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setExpiration(new Date(System.currentTimeMillis() + 200 * 1000))
+                .signWith(key)
+                .compact();
+    }
+
+    public static Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", "lcy");
+        String subject = "15662031213";
+        String token = Test.generateToken(claims, subject);
+        System.out.println(token);
+//        String token1 = "eyJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoibGN5Iiwic3ViIjoiMTU2NjIwMzEyMTMiLCJpYXQiOjE2MDI4MjczNjMsImV4cCI6MTYwMjgyNzU2M30.plscn60cpCpS2T84GddgtcrKdbNLzTcto8uX8LANgnYNsTzDTrqeLI7HXXNnwFstu1GiFvDwHpDye-Wde-iV_A";
+//        Claims claims1 = Test.getClaims(token1);
+//        System.out.println(claims1.getSubject());
     }
 
 }
